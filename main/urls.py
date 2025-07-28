900d2a0461e94370ae2a498de1445de0f5b415e0
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-07-28 22:19:33 +0100

    Install summernote

--- main/urls.py
+++ main/urls.py
@@ -21,5 +21,6 @@ from gigs.views import my_gigs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('gigs/', my_gigs, name='gigs'),
 ]
