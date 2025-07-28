82cd79d1f796d1a9ed5deeb12da70ea919ae2c38
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-07-28 21:30:35 +0100

    Set up keys

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_UoOqbRAv0i3k@ep-winter-hall-a2qqbwxl.eu-central-1.aws.neon.tech/ouch_crook_flint_60528")
+
+os.environ.setdefault("SECRET_KEY", "jb6h7ds6hb3hfb4kd7snm3bdjsiuem9d")   
