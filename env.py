d25718db71fb2ef6a5cde92b8262e73fe2ed53f3
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-08-03 15:37:49 +0100

    Set up cloudinary

--- env.py
+++ env.py
@@ -4,3 +4,6 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_UoOqbRAv0i3k@ep-winter-hall-a2qqbwxl.eu-central-1.aws.neon.tech/ouch_crook_flint_60528")
 
 os.environ.setdefault("SECRET_KEY", "jb6h7ds6hb3hfb4kd7snm3bdjsiuem9d")   
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dxhvr1vqk")
