7ed6ce8723c9c72cca7cf3ed865d951064d17cd6
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-08-03 17:13:55 +0100

    Add gig detail template

--- gigs/urls.py
+++ gigs/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
-from .views import gigs_dashboard
+from .views import (
+    gigs_dashboard,
+    GigListView,
+    GigCreateView,
+    GigDetailView,
+    GigUpdateView,
+    GigDeleteView,
+)
 
 urlpatterns = [
-    path('', gigs_dashboard, name='dashboard'),]
+    path('', gigs_dashboard, name='dashboard'),            # /dashboard/
+    path('gigs/', GigListView.as_view(), name='gig_list'),  # /dashboard/gigs/
+    path('gigs/new/', GigCreateView.as_view(), name='gig_create'),  # /dashboard/gigs/new/
+    path('gigs/<int:pk>/', GigDetailView.as_view(), name='gig_detail'),  # /dashboard/gigs/<pk>/
+    path('gigs/<int:pk>/edit/', GigUpdateView.as_view(), name='gig_edit'),  # /dashboard/gigs/<pk>/edit/
+    path('gigs/<int:pk>/delete/', GigDeleteView.as_view(), name='gig_delete'),  # /dashboard/gigs/<pk>/delete/
+]
