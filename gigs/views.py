4f260f660cc585c6ce4a3f1cbe45ab2e0ff50b3d
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-08-03 15:10:53 +0100

    Set up main logged in dashboard view

--- gigs/views.py
+++ gigs/views.py
@@ -1,7 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Gig
 
 # Create your views here.
 
+# Main dashboard My Gig Vault view
+@login_required
 def gigs_dashboard(request):
-    return render(request, "gigs/dashboard.html")
+    attended_gigs = Gig.objects.filter(user=request.user).order_by('-date')[:50]
+    upcoming_gigs = Gig.objects.filter(user=request.user, status='upcoming').order_by('date')[:50]
+
+
+    return render(request, "gigs/dashboard.html", {
+        "attended_gigs": attended_gigs,
+        "upcoming_gigs": upcoming_gigs
+    })
