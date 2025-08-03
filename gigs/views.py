4ffe70c8abc4cf7715ab2a429202354c2e84e814
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-08-03 17:13:55 +0100

    Add gig detail template

--- gigs/views.py
+++ gigs/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Gig
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
-# Main dashboard My Gig Vault view
+# Main dashboard My Gig Vault view - Function based view for dashboard
 @login_required
 def gigs_dashboard(request):
     attended_gigs = Gig.objects.filter(user=request.user).order_by('-date')[:50]
@@ -15,3 +18,14 @@ def gigs_dashboard(request):
         "attended_gigs": attended_gigs,
         "upcoming_gigs": upcoming_gigs
     })
+
+# Class based views for CRUD implementations
+
+# Gig list view
+class GigListView(LoginRequiredMixin, ListView):
+    model = Gig
+    template_name = 'gigs/gig_list.html'
+    context_object_name = 'gigs'
+
+    def get_queryset(self):
+        return Gig.objects.filter(user=self.request.user).order_by('-date')
