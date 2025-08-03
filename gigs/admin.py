ca740ad1a12194692500f7587868bf03e588947b
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-08-03 12:24:52 +0100

    Register models in admin and migrate

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Band, Venue, Gig
 
 # Register your models here.
+admin.site.register(Band)
+admin.site.register(Venue)
+admin.site.register(Gig)
--- gigvault/settings.py
+++ gigvault/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
 '127.0.0.1',]
@@ -140,6 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
