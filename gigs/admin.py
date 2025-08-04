3e10ee6895f8cda92eb8e5fee6ae71f1b44d7efa
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-08-04 18:38:27 +0100

    Improve admin search and ordering

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Band, Venue, Gig
 
-# Register your models here.
-admin.site.register(Band)
-admin.site.register(Venue)
-admin.site.register(Gig)
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')  # assuming you added location field
+    search_fields = ('name', 'location')
+    ordering = ('name',)
+
+@admin.register(Gig)
+class GigAdmin(admin.ModelAdmin):
+    list_display = ('band', 'venue', 'date')
+    search_fields = ('band__name', 'venue__name')
+    list_filter = ('date',)
+    ordering = ('-date',)
